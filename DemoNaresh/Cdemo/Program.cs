bee1044d3de41ddda2421443b6f5f94daba4fcc6
kstwkiv <sathwikakurma1@gmail.com>
2026-02-11 01:28:02 +0530

    Constructors

---
+++ DemoNaresh/Cdemo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Cdemo{
+    class ExConst{
+    public ExConst()
+        {
+            Console.WriteLine("Cosntructor is called");
+        }
+    }
+    class Program
+{
+    static void Main()
+    {
+        ExConst p=new ExConst();
+        ExConst p1=new ExConst();
+        ExConst p2=new ExConst();
+    }
+}
+}
