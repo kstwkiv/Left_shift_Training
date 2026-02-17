48892d212f9fe6ecc51d0fafbbdf5dc25c2cf17e
kstwkiv <sathwikakurma1@gmail.com>
2026-02-17 23:35:05 +0530

    ittechgenie practice questions

---
+++ ITTECHGENIE/CourseRemoval/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> courses = new Dictionary<string, int>
+        {
+            { "CSharp", 30 },
+            { "SQL", 28 },
+            { "Azure", 15 }
+        };
+
+        string cancelledCourse = Console.ReadLine();
+        // TODO: Remove key if available and print remaining courses
+        if (courses.Remove(cancelledCourse))
+        {
+            Console.WriteLine("Cancelled Course Removed");
+        }
+        else
+        {
+            Console.WriteLine("Not Found");
+        }
+        foreach(KeyValuePair<string,int> kvp in courses)
+        {
+            Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+        }
+    }
+}
