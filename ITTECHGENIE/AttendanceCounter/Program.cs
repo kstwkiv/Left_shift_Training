44567b43610a00777ee514391f0a5db131aa633a
kstwkiv <sathwikakurma1@gmail.com>
2026-02-17 22:49:14 +0530

    ittechgenie practice questions

---
+++ ITTECHGENIE/AttendanceCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] employeeIds = { 1001, 1002, 1001, 1003, 1002, 1001 };
+        Dictionary<int, int> attendance = new Dictionary<int, int>();
+        // TODO: Build frequency map and print
+        //HashSet<int>freq=new HashSet<int>();
+        for(int i = 0; i < employeeIds.Length; i++)
+        {
+            int id=employeeIds[i];
+            if (attendance.ContainsKey(id))
+            {
+                attendance[id]+=1;
+            }
+            else
+            {
+                attendance[id]=1;
+            }
+        }
+        foreach(KeyValuePair<int,int> kvp in attendance)
+        {
+            Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+        }
+    }
+}
